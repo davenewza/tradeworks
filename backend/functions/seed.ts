b05ea5d8fc7f503ff9d0c71c3a2f7020b9ced871
Dave New <sanodn@gmail.com>
2025-08-08 18:46:28 +0200

    Remove seed ts file

--- backend/functions/seed.ts
+++
@@ -1,6 +0,0 @@
-import { Seed } from '@teamkeel/sdk';
-
-// To learn more about what you can do with custom functions, visit https://docs.keel.so/functions
-export default Seed(async (ctx, inputs) => {
-
-});
