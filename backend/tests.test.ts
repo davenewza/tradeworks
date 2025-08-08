1c562616b1cd3138472c5db9612a8c68a7f6479d
Dave New <sanodn@gmail.com>
2025-08-08 07:17:37 +0200

    Removing test file

--- backend/tests.test.ts
+++
@@ -1,9 +0,0 @@
-import { actions, models, resetDatabase } from "@teamkeel/testing";
-import { test, expect, beforeEach } from "vitest";
-
-beforeEach(resetDatabase);
-
-test("basic test", async () => {
-  const created = await models.brand.create({ name: "Test" });
-  expect(created.name).toBe("Tes1");
-});
